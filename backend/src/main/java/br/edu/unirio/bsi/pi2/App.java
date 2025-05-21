6f949d7042f68ce65854a07f0bf02af226d597c6
Jenifer <jenifergonbrito@gmail.com>
2025-05-21 15:44:10 -0300

    iniciando pastas do frontend com nodejs e backend com maven

---
+++ backend/src/main/java/br/edu/unirio/bsi/pi2/App.java
@@ -0,0 +1,13 @@
+package br.edu.unirio.bsi.pi2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
