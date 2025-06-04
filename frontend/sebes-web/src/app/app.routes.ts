f339997c9eca69fa8f3fa3a556be4207445ad9ca
araujobtc <isabelletecn@gmail.com>
2025-06-03 22:42:08 -0300

    Exemplo de routes

--- frontend/sebes-web/src/app/app.routes.ts
+++ frontend/sebes-web/src/app/app.routes.ts
@@ -1,8 +1,33 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { CadastroAlunoComponent } from './cadastro/cadastro-aluno/cadastro-aluno.component';
 import { HeaderComponent } from './componentes-gerais/navbar/navbar.component';
 
+// páginas
+import { LoginComponent } from './login/login.component';
+import { CadastroAlunoComponent } from './cadastro/cadastro-aluno/cadastro-aluno.component';
+import { EditaisComponent } from './editais/editais.component';
+import { EditalComponent } from './editais/edital/edital.component';
+
 export const routes: Routes = [
-    
+    {
+		path: "",
+		component: EditaisComponent // fazer uma home, se não toda vez que acessar a pagina vai para os editais
+	},
+	{
+		path: "login"
+		component: LoginComponent
+	},
+	{
+		path: "cadastroAluno"
+		component: CadastroAlunoComponent
+	},
+	{
+		path: "editais"
+		component: EditaisComponent
+		children:[
+			{
+				path: "edital"
+				component: EditalComponent
+			}
+		]
+	}
 ];
