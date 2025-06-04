27797e7ac84a9b3a9ad04c28c66f0aa161866a04
Bárbara <babimig230@gmail.com>
2025-06-04 04:05:20 -0300

    CSS

--- frontend/sebes-web/src/app/app.component.ts
+++ frontend/sebes-web/src/app/app.component.ts
@@ -4,10 +4,12 @@ import { NavbarComponent } from "./components/shared/navbar/navbar.component";
 import { SidebarComponent } from "./components/shared/sidebar/sidebar.component";
 import { BolsaComponent } from './components/bolsas/bolsa/bolsa.component';
 import { BolsasComponent } from "./components/bolsas/bolsas.component";
+import { InscricaoComponent } from "./components/discente/inscricao/inscricao.component";
+import { InscricoesComponent } from "./components/editais/inscricoes/inscricoes.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, BolsaComponent, BolsasComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent, BolsaComponent, BolsasComponent, InscricaoComponent, InscricoesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
