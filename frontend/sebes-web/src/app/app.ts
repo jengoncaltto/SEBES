d282ac464e684f76e1eb26da6f034ce5e44f8029
ifaraujo <ifaraujo@finep.gov.br>
2025-06-06 08:38:19 -0300

    remoção de link para stylesheet e display flex do body

--- frontend/sebes-web/src/app/app.ts
+++ frontend/sebes-web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
