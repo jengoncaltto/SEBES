c78c676ea8d88d3db96da9ad045b1dbe33d9134a
araujobtc <isabelletecn@gmail.com>
2025-06-07 17:55:42 -0300

    listagem de bolsas

--- frontend/sebes-web/src/app/pages/bolsas/bolsas.ts
+++ frontend/sebes-web/src/app/pages/bolsas/bolsas.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { DivBolsaComponent } from "./div-bolsa/div-bolsa.component";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { BolsaResumidaDTO } from '../../core/models/BolsaResumidaDTO';
 
 @Component({
   selector: 'app-bolsas',
-  imports: [DivBolsaComponent],
+  imports: [CommonModule, RouterLink],
   templateUrl: './bolsas.html',
   styleUrl: './bolsas.css'
 })
 export class Bolsas {
+    bolsas: BolsaResumidaDTO[] = [];
+    constructor() {
+        // Criando bolsas de exemplo
+        const bolsa1 = new BolsaResumidaDTO('Bolsa Mérito', 'Ajuda alunos testando o tamanho do texto pra ver se fica menor e o limite com ótimo desempenho.');
+        const bolsa2 = new BolsaResumidaDTO('Bolsa Pesquisa', 'Para projetos de iniciação científica.');
+        const bolsa3 = new BolsaResumidaDTO('Bolsa Mérito', 'Ajuda alunos com ótimo desempenho.');
+        const bolsa4 = new BolsaResumidaDTO('Bolsa Pesquisa', 'Para projetos de iniciação científica.');
 
+        this.bolsas.push(bolsa1, bolsa2, bolsa3, bolsa4, bolsa3, bolsa4);
+    }
 }
