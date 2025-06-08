24778eb7852868432addccc1aaa7b32a42279328
Jenifer <jenifergonbrito@gmail.com>
2025-06-07 21:31:31 -0300

    pagina-inicial.html codada e anexação da rota dela no navbar e ao iniciar

--- frontend/sebes-web/src/app/pages/pagina-inicial/pagina-inicial.ts
+++ frontend/sebes-web/src/app/pages/pagina-inicial/pagina-inicial.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './pagina-inicial.html',
   styleUrl: './pagina-inicial.css'
 })
--- frontend/sebes-web/src/app/shared/components/navbar/navbar.ts
+++ frontend/sebes-web/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [ RouterModule ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  showSideBar = true;
 
+  constructor(private router: Router){
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe((event: NavigationEnd)=> (
+      this.showSideBar = event.urlAfterRedirects !== '/pagina-inicial'
+    ));
+  }
 }
