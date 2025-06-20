61f5610d618fa367b55e62c4133d64be820dabbc
Bárbara <babimig230@gmail.com>
2025-06-20 12:44:26 -0300

    Adição do inscricoesDTO, mudanças no css e reescrita do html de detalhes-inscrição

--- frontend/sebes-web/src/app/core/models/InscricaoResumidaDTO.ts
+++ frontend/sebes-web/src/app/core/models/InscricaoResumidaDTO.ts
@@ -1,1 +1,22 @@
-export interface InscricaoResumidaDTO { }
+export class InscricaoResumidaDTO {
+    constructor(
+      private nomeDocumento: string,
+      private status:string,
+    ) {}
+
+    getNomeDocumento(): string {
+      return this.nomeDocumento;
+    }
+
+    setNomeDocumento(value: string) {
+      this.nomeDocumento = value;
+    }
+
+    getStatus(): string {
+      return this.status;
+    }
+
+    setStatus(value: string) {
+      this.status = value;
+    }
+}
--- frontend/sebes-web/src/app/pages/processoSeletivo/detalhes-inscricao/detalhes-inscricao.ts
+++ frontend/sebes-web/src/app/pages/processoSeletivo/detalhes-inscricao/detalhes-inscricao.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { InscricaoResumidaDTO } from '../../../core/models/InscricaoResumidaDTO';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-detalhes-inscricao',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './detalhes-inscricao.html',
   styleUrl: './detalhes-inscricao.css'
 })
 export class DetalhesInscricao {
+  inscricoes : InscricaoResumidaDTO[]=[];
+  constructor(){
+      
+  const inscricao1 = new InscricaoResumidaDTO('Comprovante de Renda', 'Enviado');
+  const inscricao2 = new InscricaoResumidaDTO('Comprovante de Residência', 'Pendente');
+  const inscricao3 = new InscricaoResumidaDTO('Documento', 'Enviado');
+  const inscricao4 = new InscricaoResumidaDTO('Documento', 'Pendente');
+
+  this.inscricoes.push(inscricao1, inscricao2, inscricao3, inscricao4, inscricao3, inscricao4);
+  }
 
 }
