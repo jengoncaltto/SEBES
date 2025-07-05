7c139186e81f5c48559798d3436048bf0f5bdbee
Jenifer <jenifergonbrito@gmail.com>
2025-07-04 23:04:08 -0300

    principais dtos angular

---
+++ frontend/sebes-web/src/app/core/models/dtos/discente.dto.ts
@@ -0,0 +1,10 @@
+export interface DiscenteDto {
+  id: string;
+  nome: string;
+  nomeSocial: string | null;
+  email: string;
+  emailRecuperacao: string;
+  telefone: string;
+  matricula: string;
+  idUsuario: string;
+}
---
+++ frontend/sebes-web/src/app/core/models/dtos/servidor-prae.dto.ts
@@ -0,0 +1,11 @@
+export interface ServidorPraeDto {
+  id: string;
+  nome: string;
+  nomeSocial: string;
+  email: string;
+  emailRecuperacao: string;
+  telefone: string;
+  setor: string;
+  cargo: string;
+  idUsuario: string;
+}
