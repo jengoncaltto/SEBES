0cdfddeb3ffaa7cd2121455fdf69cc9b58744a54
BarbaraMig <babimig230@gmail.com>
2025-07-08 10:58:13 -0300

    Integração processosSeletivos .ts

--- frontend/sebes-web/src/app/pages/processoSeletivo/processos-seletivos/processos-seletivos.ts
+++ frontend/sebes-web/src/app/pages/processoSeletivo/processos-seletivos/processos-seletivos.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+import { ProcessoSeletivoDto } from '@models/dtos/processo-seletivo.dto';
+import { ProcessoSeletivoService } from '@services/processo.service';
 
 
 
 @Component({
   selector: 'app-processos-seletivos',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './processos-seletivos.html',
   styleUrl: './processos-seletivos.css'
 })
-export class ProcessosSeletivos {
+export class ProcessosSeletivos implements OnInit{
+  processos:ProcessoSeletivoDto[]=[];
 
+  constructor(private processoService:ProcessoSeletivoService){}
+
+  ngOnInit(): void {
+    this.carregarProcessos();
+  }
+  private carregarProcessos():void{
+    this.processoService.listarTodos().subscribe({
+      next: (dados:ProcessoSeletivoDto[]) => {
+        this.processos=dados;
+      },
+      error:(err : any) => {
+        console.log('Erro ao carregar processos', err);
+      }
+    })
+  }
 }
