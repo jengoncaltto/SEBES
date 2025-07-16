b1e8e5d52f8c9083a85f52f56a57d1113c5a6ef8
ifaraujo <ifaraujo@finep.gov.br>
2025-07-16 13:30:51 -0300

    configuracao de requisicao

--- backend/src/main/java/br/uniriotec/prae/sebes/config/CorsConfig.java
+++ backend/src/main/java/br/uniriotec/prae/sebes/config/CorsConfig.java
@@ -13,10 +13,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:4200")
-                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
