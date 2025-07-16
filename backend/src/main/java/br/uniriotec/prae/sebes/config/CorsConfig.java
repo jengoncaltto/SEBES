7592b7ad1d347d51cf1341ea065b02f48dcd4014
ifaraujo <ifaraujo@finep.gov.br>
2025-07-16 13:36:16 -0300

    allowedOrigins

--- backend/src/main/java/br/uniriotec/prae/sebes/config/CorsConfig.java
+++ backend/src/main/java/br/uniriotec/prae/sebes/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://sebes-rose.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
