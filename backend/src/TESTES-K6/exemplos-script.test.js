178853f9ea32286caaaadf1ae3e86c9d7fc5e04c
BarbaraMig <babimig230@gmail.com>
2025-07-16 10:13:31 -0300

    Mudanças nos testes

--- backend/src/TESTES-K6/exemplos-script.test.js
+++ backend/src/TESTES-K6/exemplos-script.test.js
@@ -6,18 +6,18 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 //Comando resultado em JSON : k6 run exemplos-script.test.js --out json=resultados-json/resultado.json   
 //Comando resultado em Web-Dashboard : k6.exe run --out web-dashboard=report.html exemplos-script.test.js
 
-//Configurações de número de virtual users(VUS) e duração
+//Configurações de número de virtual users(VUS) e duração das fases de teste
 export let options = {
     stages: [
-      /*
-    { duration: '40s', target: 50 }, //Ramp-Up
-    { duration: '1m', target: 50 },  // Mantém por 1 min
-    { duration: '30s', target: 100 }, // Pico 
-    { duration: '1m', target: 100 },  // Sustenta o pico
+    
+    { duration: '30s', target: 20 }, //Ramp-Up
+    { duration: '1m', target: 20 },  // Mantém por 1 min
+    { duration: '30s', target: 40 }, // Pico 
+    { duration: '1m', target: 40 },  // Sustenta o pico
     { duration: '1m', target: 0 },  // Reduz para 0
-     */
-    //Teste de chamada
-    { duration: '10s', target: 10 }, //Ramp-Up
+     
+    //Teste de chamada, conferir o funcionamento do endpoint
+    //{ duration: '10s', target: 10 },
 
   ]
 };
@@ -25,13 +25,13 @@ export let options = {
 //Cada VUS executará a função simultaneamente
 export default function (){
     //Seleção de qual requisição testar
+    testeGETBolsas();
+    /*
+    
+    testePOSTBolsas();
+    */
+   //Extra
     //testeGETDiscente();
-    testePOSTDiscente();
-    //testeGETBolsas();
-    //testePOSTBolsas();
-
-    //Pausa de 1s até a próxima requisição
-    sleep(1);
 }
 
 export function testeGETBolsas(){
@@ -44,6 +44,9 @@ export function testeGETBolsas(){
         //se o tempo total da requisição for maior que o especificado, a verificação falha
         'latência < 500ms': (r) => r.timings.duration < 500,
     });
+    
+    //Pausa de 1s até a próxima requisição
+    sleep(1);
        
 }
 
@@ -57,6 +60,9 @@ export function testeGETDiscente(){
         //se o tempo total da requisição for maior que o especificado, a verificação falha
         'latência < 500ms': (r) => r.timings.duration < 500,
     });
+
+    //Pausa de 1s até a próxima requisição
+    sleep(1);
        
 }
 
@@ -79,34 +85,12 @@ export function testePOSTBolsas(){
     'status é 201': (r) => r.status === 201,
     'latência < 500ms': (r) => r.timings.duration < 500,
   });
+  //Pausa de 1s até a próxima requisição
+    sleep(1);
 
 }
 
-export function testePOSTDiscente(){
-  varEmail= new Date;
-    //"Constante" utilizada para facilitar testes
-    usuario = JSON.stringify({
-      nomeUsuario: 'A',
-      nomeCompletoDiscente: 'Aluno Teste',
-      nome_social: '',
-      matricula: '12345678910',
-      telefone: '1234567891011121',
-      email: `${varEmail}@edu.unirio.br`
-  });
-  
-  
-  //Define o tipo de conteúdo do corpo da requisição
-  const headers = { 'Content-Type': 'application/json' };
 
-  const res = http.post('https://sebes-weck.onrender.com/api/discentes/cadastrar', usuario, { headers });
-
-  check(res, {
-    //201 indica que o POST foi bem sucedido
-    'status é 201': (r) => r.status === 201,
-    'latência < 500ms': (r) => r.timings.duration < 500,
-  });
-
-}
 
 //Configurações  do Dashboard
 export function handleSummary(data) {
