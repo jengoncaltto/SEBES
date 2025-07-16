39fd251b5a2ab5662abb3091955d65c6b0128c9a
ifaraujo <ifaraujo@finep.gov.br>
2025-07-16 13:55:54 -0300

    menu lateral

--- frontend/sebes-web/src/app/shared/components/sidebar/sidebar.ts
+++ frontend/sebes-web/src/app/shared/components/sidebar/sidebar.ts
@@ -13,16 +13,17 @@ import { AuthService } from '@services/auth.service';
 export class Sidebar{
   isDiscente = true;
   textoOpcaoInscricoes = "";
-  isLogado = true;
 
-  constructor(private authService : AuthService) {
-    //Ta errado isso ainda
-      if(this.authService.getTipo()?.includes('discente')){
+  constructor(private authService : AuthService) {}
+  	isLoggedIn = this.authService.isLoggedIn();
+  
+    if(this.authService.getTipo()?.includes('discente')){
       this.textoOpcaoInscricoes = "Minhas inscrições";
     } else{
       this.textoOpcaoInscricoes = "Inscrições Recebidas";
     }
-    //Colocar um else caso tipo seja null??
     
+  logout(){
+  	this.authService.logout();
   }
 }
